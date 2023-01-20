0648a5ba411bcec1d543b8bbd5b822d41f403428
Sam M <samuel.merlin@stvincent.edu>
2023-01-19 20:00:33 -0500

    The changes
    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-function App() {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function FormRow() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+    </React.Fragment>
   );
 }
 
-export default App;
+
+export default function app() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
