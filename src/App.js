7622b77974b0ed976525fb5fde1c8a4ab3b1900b
Sam M <samuel.merlin@stvincent.edu>
2023-01-22 14:30:43 -0500

    Commit1/22/2023

--- src/App.js
+++ src/App.js
@@ -1,48 +1,75 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(12),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
-function FormRow() {
+  function FormRowOne() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Item>Uno</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Dos</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Tres</Item>
+        </Grid>
+      </React.Fragment>
+);}
+
+function FormRowTwo() {
   return (
     <React.Fragment>
       <Grid item xs={4}>
-        <Item>Item</Item>
+        <Item>One</Item>
       </Grid>
       <Grid item xs={4}>
-        <Item>Item</Item>
+        <Item>Two</Item>
       </Grid>
       <Grid item xs={4}>
-        <Item>Item</Item>
+        <Item>Three</Item>
       </Grid>
     </React.Fragment>
-  );
-}
+);}
 
-
-export default function app() {
+function FormRowThree() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1}>
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>Ein</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Zwei</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Drei</Item>
+      </Grid>
+    </React.Fragment>
+);}
+
+export default function NestGird() {
+  return (
+      <Grid container direction="row" justifyContent="center" alignItems="center" spacing={1}>
         <Grid container item spacing={3}>
-          <FormRow />
+        <FormRowOne />
         </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
+        <Grid container item spacing={3} >
+        <FormRowTwo />
         </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
+        <Grid container item spacing={3} >
+        <FormRowThree />
         </Grid>
       </Grid>
-    </Box>
-  );
-}
+  )
+};
---
+++ src/Form.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function () {
+  return (
+      <div><h1> Hello World</h1></div>
+  )
+};
